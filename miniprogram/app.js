32888073d88fe948a9cb4fe8f65b5ee893a3b92f
shuaizi <77367582@qq.com>
2020-12-20 15:25:54 +0800

    小程序检测新版本

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,6 +1,10 @@
 //app.js
 App({
-  onLaunch: function () {
+  onLaunch: function (options) {
+    // 小程序进入方式
+    console.log(options)
+    // 检测新版本
+    this.checkUpate()
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -40,4 +44,23 @@ App({
       }
     })
   },
+  checkUpate(){
+    const updateManager = wx.getUpdateManager()
+    // 检测版本更新
+    updateManager.onCheckForUpdate((res)=>{
+      if (res.hasUpdate){
+        updateManager.onUpdateReady(()=>{
+          wx.showModal({
+            title: '更新提示',
+            content: '新版本已经准备好，是否重启应用',
+            success(res){
+              if(res.confirm){
+                updateManager.applyUpdate()
+              }
+            }
+          })
+        })
+      }
+    })
+  }
 })
